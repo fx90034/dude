5ceae3bd66cb5a676f43b148e12661d07a07c307
fx90034 <fx90034@gmail.com>
2018-12-19 15:44:35 -0800

    added level4 routes.

--- server/routes/apps/index.js
+++ server/routes/apps/index.js
@@ -59,7 +59,6 @@ debug('i = ' + i)
 debug('j = ' + j)
 debug('level1 = ' + level1)
 debug('level2 = ' + level2)
-  var answers = [];
   util.getLevel3(i, j, function(err, data) {
     if(err) {
       console.error(err);
@@ -76,9 +75,6 @@ debug("user.name = " + user.name)
 
 router.get('/level4', function(req, res) {
   let user = null;
-  let j = req.query.j;
-  let k = req.query.k;
-  let l = req.query.l;
   let level1 = req.query.level1;
   let level2 = req.query.level2;
   let level3 = req.query.level3;
@@ -90,15 +86,34 @@ debug('ip = ' + req.ip)
 debug('level1 = ' + level1)
 debug('level2 = ' + level2)
 debug('level3 = ' + level3)
-  db.level3.addLevel3(level1, level2, level3, req.ip, user, function(err, body) {
-    if(err) {
-      console.error(err);
-      res.render(error, { message: 'Please try again.'});
-    }
-    else {
-        res.redirect(level3);
-    }
-  });
+  if(level1 === 'Devices') {
+    util.getDevices(level2, level3, function(err, data) {
+      if(err) {
+        console.error(err);
+        throw err;
+  //      return res.render('error', { error: err });
+      }
+      else {
+  debug("data[0] = " + data[0])
+      }
+  debug("user.name = " + user.name)
+      res.render('apps/list', { user: user, level1: level1, level2: level2, level3: level3, data: data });
+    });
+  }
+  else {
+    util.getDeviceByName(level3, function(err, data) {
+      if(err) {
+        console.error(err);
+        throw err;
+  //      return res.render('error', { error: err });
+      }
+      else {
+  debug("data[0] = " + data[0])
+      }
+  debug("user.name = " + user.name)
+      res.render('apps/level3', { user: user, level1: 'Devices', level2: level3, data: data });
+    });
+  }
 });
 
 router.post('/level4', function(req, res) {
