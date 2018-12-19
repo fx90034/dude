cd9446e2c23cffec4e34ee2aba4f7ef599e14ba6
fx90034 <fx90034@gmail.com>
2018-12-18 21:05:23 -0800

    changed to SysHomes.

--- server/routes/apps/util.js
+++ server/routes/apps/util.js
@@ -2,7 +2,8 @@
 
 const util = require('../util');
 const debug = require('debug')('apps_util');
-const file = './conf/apps.json';
+const appsFile = './conf/apps.json';
+const devicesFile = './conf/devices.json';
 
 var rooms = [];
 var scenes = [];
@@ -10,33 +11,12 @@ var devices = [];
 var appData = [[[]]];
 
 exports.getLevel1 = function(callback) {
-debug(appData[0][0][0])
-  if(!appData[0][0][0]) {
-debug("Loading ...")
-    load(function(err, data) {
-      if(err) {
-        console.error(err);
-        return callback(err, null);
-      }
-debug("@@@@@@@@@@Loaded.")
-debug("appData[0][0][0] = " + appData[0][0][0])
-      var temp = [];
-      for(var i=0; i<appData.length; i++) {
-        temp.push(appData[i][0][0]);
-      }
-debug("temp = " + JSON.stringify(temp))
-      return callback(null, temp);
-    });
+  var temp = [];
+  for(var i=0; i<appData.length; i++) {
+    temp.push(appData[i][0][0]);
   }
-  else {
-    var temp = [];
-    for(var i=0; i<appData.length; i++) {
-      temp.push(appData[i][0][0]);
-    }
 debug("temp = " + JSON.stringify(temp))
-    return callback(null, temp);
-  }
-debug("appData.length = " + appData.length)
+  return callback(null, temp);
 }
 exports.getLevel2 = function(i, callback) {
   var temp = [];
@@ -55,33 +35,9 @@ exports.getLevel3 = function(i, j, callback) {
 debug("temp = " + JSON.stringify(temp))
   return callback(null, temp);
 }
-exports.getData = function(i, j, k, callback) {
-debug(appData[0][0][0])
-  if(!appData[0][0][0]) {
-debug("Loading ...")
-    load(function(err, data) {
-      if(err) {
-        console.error(err);
-        return callback(err, null);
-      }
-debug("@@@@@@@@@@Loaded.")
-debug("appData[0][0][0] = " + appData[0][0][0])
-      var data = { appData: appData[i][j][k] };
-debug("data = " + JSON.stringify(data))
-      return callback(null, data);
-    });
-  }
-  else {
-debug("i=" + i)
-debug("j=" + j)
-debug("k=" + k)
-    var data = { appData: appData[i][j][k] };
-debug("data = " + JSON.stringify(data))
-    return callback(null, data);
-  }
-}
-function load(callback) {
-  util.readJSON(file, function(err, data) {
+exports.loadApps = function(callback) {
+debug("Loading Apps ...")
+  util.readJSON(appsFile, function(err, data) {
     if(err) {
       console.error(err);
       throw err;
@@ -119,6 +75,52 @@ debug("appData[i][j][k] = " + JSON.stringify(appData[i][j][k]))
       }
     } catch(ex) {
       console.error(ex);
+      return callback(ex, null);
+    }
+    return callback(null, null);
+  });
+}
+exports.loadDevices = function(callback) {
+debug("Loading Devices ...")
+  util.readJSON(devicesFile, function(err, data) {
+    if(err) {
+      console.error(err);
+      throw err;
+    }
+    try {
+      var level0 = data;
+      var level1;
+      var level2;
+      var level3;
+      var level1Keys = Object.keys(level0);
+ debug("!level1Keys = " + level1Keys)
+      for(var i=0; i<level1Keys.length; i++) { // "Rooms", "Scenes", "Devices"
+        appData[i] = [];
+        appData[i][0] = [];
+        appData[i][0][0] = level1Keys[i];
+ debug("appData[i][0][0] = " + JSON.stringify(appData[i][0][0]))
+        level1 = level0[level1Keys[i]];
+        var level2Keys = Object.keys(level1); // "Entry", "Kitchen", "Bathroom", "Bedroom", ...
+debug("level2Keys = " + level2Keys)
+        for(var j=1; j<=level2Keys.length; j++) {
+debug('j = ' + j)
+          appData[i][j] = [];
+          appData[i][j][0] = level2Keys[j-1]; // "Entry"
+debug("appData[i][j][0] = " + JSON.stringify(appData[i][j][0]))
+          level2 = level1[level2Keys[j-1]];
+debug("level2 = " +  JSON.stringify(level2))
+          var level3Keys = Object.keys(level2); // "Light", "Switch", "Lock", ...
+debug("level3Keys = " + level3Keys)
+          for(var k=1; k<=level2.length; k++) { // "Light", "Switch", "Lock", ...
+debug('k = ' + k)
+            appData[i][j][k] = level2[k-1]; // "Light"
+debug("appData[i][j][k] = " + JSON.stringify(appData[i][j][k]))
+          }
+        }
+      }
+    } catch(ex) {
+      console.error(ex);
+      return callback(ex, null);
     }
     return callback(null, null);
   });
