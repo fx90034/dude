de709d5c61be5ea6d5918c9dec9bbea946118368
bagpipes <dylanswafflez@gmail.com>
2018-12-26 15:57:15 -0800

    list page

--- server/models/init.js
+++ server/models/init.js
@@ -2,9 +2,9 @@
 
 const env = process.env.NODE_ENV || 'dev'
 const config = require('../conf/config.' + env);
-const nano = require('nano')('http://localhost:' + config.db.port);
+const nano = require('nano')('http://dylansun:shianing123@localhost:' + config.db.port);
 
 require('./users/init.js');
 require('./why/init.js');
 require('./devices/init.js');
-require('./apps/init.js');
+// require('./apps/init.js');
